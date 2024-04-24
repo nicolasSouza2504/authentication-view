1d2d339d464a2ea18f7681a3cb62a449cd80e061
nicolas <nicolas.souza@moinformatica.com>
2024-04-24 19:30:11 -0300

    first commit authentication view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const  handleLogin = async (event) => {
+
+        event.preventDefault();
+
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username: username, password: password}),
+        })
+            .then(response => response.json())
+            .then(data => {
+                return data;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                return null;
+            });
+
+        console.log(response)
+
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <div>
+                        <h2>Login React</h2>
+                        <img src={logo} className="App-logo" alt="logo"/>
+                    </div>
+                    <form onSubmit={handleLogin}>
+                        <div className={"Input-div"}>
+                            <label htmlFor="username">Username:</label>
+                            <input
+                                type="text"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className={"Input-div"}>
+                            <label htmlFor="password" >Password:</label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </header>
+        </div>
+    );
+
 }
 
 export default App;
